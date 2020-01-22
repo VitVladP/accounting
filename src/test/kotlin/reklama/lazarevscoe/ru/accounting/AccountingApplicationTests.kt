f63455f48cff8cd81eb8219855109699e60677b0
vitaly <vit.vlad.p@gmail.com>
2020-01-22 09:49:07 +0300

    start

---
+++ src/test/kotlin/reklama/lazarevscoe/ru/accounting/AccountingApplicationTests.kt
@@ -0,0 +1,13 @@
+package reklama.lazarevscoe.ru.accounting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
